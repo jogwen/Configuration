cc59d344d31b9b7302606ec192d400dccb7adee2
jogwen <joanna.wakeford+github@gmail.com>
2012-10-05 14:24:40 +0100

    Suppress error if dir already exists.

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
-mkdir ~/.vim/indent
+mkdir -p ~/.vim/indent
 ln -s $PWD/vim/indent/python.vim ~/.vim/indent/python.vim
