9871b29c3bf86e084fd5767be062534ffc5747ae
jogwen <joanna.wakeford+github@gmail.com>
2012-10-05 13:19:39 +0100

    Tidied up by removing stuff I'm not currenty using.

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#ln -s $PWD/zshrc ~/.zshrc
-#ln -s $PWD/tmux.conf ~/.tmux.conf
-
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
 mkdir ~/.vim/indent
