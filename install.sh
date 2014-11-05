b8b7591ea917f392cb3e3b23bceff8fcf299350e
jo <jo@kp-local.(none)>
2014-11-05 15:16:10 +0000

    Fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -s $PWD/vimrc ~/.vimrc
+mkdir -p ~/.vim/ftplugin
 ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
 mkdir -p ~/.vim/indent
 ln -s $PWD/vim/indent/python.vim ~/.vim/indent/python.vim
