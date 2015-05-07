778e50f3f726e06d69cfa6f5e043bcd7919508e2
jogwen <joanna.wakeford+github@gmail.com>
2015-05-07 19:08:50 +0100

    Now installs everything for vimdiff-like 'git diff', makes ~/.bashrc source our root file of bashrc customisations.

--- install.sh
+++ install.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
 
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
-mkdir -p ~/.vim/indent
-ln -s $PWD/vim/indent/python.vim ~/.vim/indent/python.vim
+#################################################################
+# bashrc customisations:
+#################################################################
+# source our main file from the current user's .bashrc (back it up first)
+echo "Creating backup before editing: ~/.bashrc.orig"
+cp -p ~/.bashrc ~/.bashrc.orig
+echo "source ~/.custom_bashrc" >> ~/.bashrc
+# Create symlinks to our sub-config files:
+ln -s $PWD/custom_bashrc ~/.custom_bashrc
+ln -s $PWD/custom_bashrc_prompt ~/.custom_bashrc_prompt
+ln -s $PWD/custom_bashrc_gitgraph ~/.custom_bashrc_gitgraph
+ln -s $PWD/custom_bashrc_gitdiff ~/.custom_bashrc_gitdiff
+
+
+#################################################################
+# finish off installing vimdiff-like 'git diff':
+#################################################################
+echo "Creating backup before editing: ~/.gitconfig.orig"
+cp -p ~/.gitconfig ~/.gitconfig.orig
+echo "[diff]\n    external = git_diff_wrapper\n[pager]\n    diff =" >> ~/.gitconfig
+mkdir -p ~/bin
+printf '#!/bin/sh\n\nvimdiff "$2" "$5"\n' > ~/bin/git_diff_wrapper
+chmod u+x ~/bin/git_diff_wrapper
+
+#################################################################
+# vim customisations:
+#################################################################
+#
+##ln -s $PWD/vimrc ~/.vimrc
+##ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
+##mkdir -p ~/.vim/indent
+##ln -s $PWD/vim/indent/python.vim ~/.vim/indent/python.vim
+#
+#
+echo "\n\nNow do \"source ~/.bashrc\" to get full effect.\n"
