ec94077858fe65fda0cce5f60f599b1b5d4d1c86
Jo Paulger <joanna.paulger@gmail.com>
2019-07-23 14:25:28 +0100

    Use .bash_profile, not .bashrc

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 #################################################################
-# bashrc customisations:
+# bash_profile customisations:
 #################################################################
 # Create symlinks to our sub-config files:
-ln -s $PWD/custom_bashrc ~/.custom_bashrc
-ln -s $PWD/custom_bashrc_prompt ~/.custom_bashrc_prompt
-ln -s $PWD/custom_bashrc_gitgraph ~/.custom_bashrc_gitgraph
-ln -s $PWD/custom_bashrc_gitdiff ~/.custom_bashrc_gitdiff
-ln -s $PWD/custom_bashrc_docker-compose ~/.custom_bashrc_docker-compose
-# Now source our main file from the current user's .bashrc (back it up first)
-echo "Creating backup before editing: ~/.bashrc.orig"
-touch ~/.bashrc
-cp -p ~/.bashrc ~/.bashrc.orig
-echo "source ~/.custom_bashrc" >> ~/.bashrc
+ln -s $PWD/custom_bash_profile ~/.custom_bash_profile
+ln -s $PWD/custom_bash_profile_prompt ~/.custom_bash_profile_prompt
+ln -s $PWD/custom_bash_profile_gitgraph ~/.custom_bash_profile_gitgraph
+ln -s $PWD/custom_bash_profile_gitdiff ~/.custom_bash_profile_gitdiff
+ln -s $PWD/custom_bash_profile_docker-compose ~/.custom_bash_profile_docker-compose
+# Now source our main file from the current user's .bash_profile (back it up first)
+echo "Creating backup before editing: ~/.bash_profile.orig"
+touch ~/.bash_profile
+cp -p ~/.bash_profile ~/.bash_profile.orig
+echo "source ~/.custom_bash_profile" >> ~/.bash_profile
 
 #################################################################
 # Install binaries:
@@ -38,4 +38,4 @@ ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
 mkdir -p ~/.vim/indent
 ln -s $PWD/vim/indent/python.vim ~/.vim/indent/python.vim
 
-echo "\n\nNow run \"source ~/.bashrc\" to complete installation.\n"
+echo "\n\nNow run \"source ~/.bash_profile\" to complete installation.\n"
